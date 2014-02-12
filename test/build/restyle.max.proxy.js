622fa21aca76c95afb21a41108f7208813bcf5e9
Andrea Giammarchi <agiammarchi@twitter.com>
2014-02-11 21:39:04 -0800

    automatic test generator

--- test/build/restyle.max.proxy.js
+++ test/build/restyle.max.proxy.js
@@ -146,16 +146,40 @@ var utils = function(){
 
 var proxyHandler = {
   has: function (target, name) {
-    return  utils.$global.hasOwnProperty(name) ?
-        utils.$clash.test(name) : true;
+    return name[0] === '$' || (
+      utils.$global.hasOwnProperty(name) ?
+        utils.$clash.test(name) :
+        true
+    );
+  },
+  set: function (target, name, value, receiver) {
+    target[name] = value;
   },
   get: function (target, name, receiver) {
-    return name[0] !== '$' && utils.hasOwnProperty(name) ?
-      utils[name] :
-      utils.$unCamel(name)
-    ;
+    return name[0] === '$' ?
+      target[name] || utils.$global[name] : (
+      utils.hasOwnProperty(name) ?
+        utils[name] :
+        utils.$unCamel(name)
+    );
   }
 };
+
+/* i.e.
+with (restyle.proxy) {
+
+  // some variable declaration
+  $color = '#ABC';
+
+  // style to return
+  ({
+    body: {
+      color: $color
+    }
+  });
+}
+*/
+
 
 return new Proxy(
   Object.create(null),
