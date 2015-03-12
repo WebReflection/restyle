51f36aab3b7e98e1de48a66f6848dfb56ba21b5a
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2015-03-12 15:26:00 +0000

    automatic test generator

--- test/build/restyle.max.js
+++ test/build/restyle.max.js
@@ -206,6 +206,102 @@ var restyle = (function (O) {
     };
   }
 
+  // bringing animation utility in window-aware world only
+  if (!{undefined: true}[typeof window]) {
+    restyle.animate = (function (g) {
+
+      var type;
+      switch (true) {
+        case !!g.AnimationEvent:
+          type = 'animationend';
+          break;
+        case !!g.WebKitAnimationEvent:
+          type = 'webkitAnimationEnd';
+          break;
+        case !!g.MSAnimationEvent:
+          type = 'MSAnimationEnd';
+          break;
+        case !!g.OAnimationEvent:
+          type = 'oanimationend';
+          break;
+      }
+
+      ReStyle.prototype.getAnimationDuration = function (el, name) {
+        for (var
+          chunk, duration,
+          classes = el.className.split(/\s+/),
+          i = classes.length; i--;
+        ) {
+          chunk = classes[i];
+          if (
+            chunk.length &&
+            (new RegExp('\\.' + chunk + '(?:|\\{|\\,)([^}]+?)\\}')).test(this.css)
+          ) {
+            chunk = RegExp.$1;
+            if (
+              (new RegExp(
+                'animation-name:' +
+                name +
+                ';.*?animation-duration:([^;]+?);'
+              )).test(chunk) ||
+              (new RegExp(
+                'animation:\\s*' + name + '\\s+([^\\s]+?);'
+              )).test(chunk)
+            ) {
+              chunk = RegExp.$1;
+              duration = parseFloat(chunk);
+              if (duration) {
+                return duration * (/[^m]s$/.test(chunk) ? 1000 : 1);
+              }
+            }
+          }
+        }
+        return -1;
+      };
+
+      ReStyle.prototype.animate = type ?
+        function animate(el, name, callback) {
+          function onAnimationEnd(e) {
+            if (e.animationName === name) {
+              drop();
+              callback.call(el, e);
+            }
+          }
+          function drop() {
+            el.removeEventListener(type, onAnimationEnd, false);
+          }
+          el.addEventListener(type, onAnimationEnd, false);
+          return {drop: drop};
+        } :
+        function animate(el, name, callback) {
+          var i, drop, duration = this.getAnimationDuration(el, name);
+          if (duration < 0) {
+            drop = O;
+          } else {
+            i = setTimeout(
+              function () {
+                callback.call(el, {
+                  type: 'animationend',
+                  animationName: name,
+                  currentTarget: el,
+                  target: el,
+                  stopImmediatePropagation: O,
+                  stopPropagation: O,
+                  preventDefault: O
+                });
+              },
+              duration
+            );
+            drop = function () {
+              clearTimeout(i);
+            };
+          }
+          return {drop: drop};
+        }
+      ;
+    }(window));
+  }
+
   restyle.customElement = function (name, constructor, proto) {
     var key, prototype = Object.create(constructor.prototype);
     if (proto && proto.css) {
